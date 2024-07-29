779de57f0671d3e8f1cc886c12e188e9f0c3ff66
energeticelephant <energeticelephant1998@gmail.com>
2024-07-30 01:07:14 +0800

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Box, Container, Typography, IconButton, Grid, Link } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 import TwitterIcon from "@mui/icons-material/Twitter";
+import EmailIcon from "@mui/icons-material/Email";
 
 const Footer = () => {
   return (
@@ -23,7 +24,7 @@ const Footer = () => {
         >
           <Grid item xs={12} sm={4} textAlign="center">
             <Typography variant="body1">Follow us on social media</Typography>
-            <IconButton href="https://twitter.com/theplantifyshop" target="_blank">
+            <IconButton href="https://twitter.com/wedgerbets" target="_blank">
               <TwitterIcon />
             </IconButton>
           </Grid>
@@ -34,21 +35,30 @@ const Footer = () => {
                 to="/"
                 style={{ textDecoration: "none", color: "inherit" }}
               >
-                PLANTIFY
+                BETWAGER
               </RouterLink>{" "}
               {new Date().getFullYear()}
               {"."}
             </Typography>
           </Grid>
           <Grid item xs={12} sm={4} textAlign="center">
-            <Link>
-              {" "}
-              <RouterLink
-                to="/aboutus"
-                style={{ color: "inherit", margin: "0 8px" }}
-              >
-                About Us
-              </RouterLink>
+            <Link
+              component={RouterLink}
+              to="/aboutus"
+              style={{ color: "inherit", margin: "0 8px" }}
+            >
+              About Us
+            </Link>
+            <Link
+              href="mailto:sales@plantify.dev"
+              style={{ color: "inherit", margin: "0 8px" }}
+              onClick={(e) => {
+                e.preventDefault();
+                window.location.href = "mailto:sales@plantify.dev";
+              }}
+            >
+              <EmailIcon sx={{ verticalAlign: 'middle', mr: 0.5 }} />
+              sales@plantify.dev
             </Link>
           </Grid>
         </Grid>
